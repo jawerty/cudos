661cfcbd550b7a0d1aa0e06cc2a5d8a8e754a6cb
jawerty <jawerty210@gmail.com>
2014-03-23 05:33:00 -0400

    prototype

--- routes/categories.js
+++ routes/categories.js
@@ -1,34 +1,35 @@
 mongoose = require("mongoose")
 require( '../db' );
 site = mongoose.model('site')
+    
+exports.index = function(req, res) {
+	page = req.query.page || 0
+	skipNum = parseInt(page) * 25 + 1  || 0
 
-exports.index = function(res, req) {
 	category = req.params.category;
-	type = req.params.type
-
+	type = req.params.type;
+    
 	if (typeof type == "undefined") {
 		type = "top"
 	}
-
-	page = req.query.page
-	skipNum = page + 1 * 25 || 0
-
+    
 	if (type == "top") {
 		t = "cudos"
 	} else if (type == "new") {
 		t = "date"
 	}
-
-	site.find({}).sort('-'+t).limit(25).skip(skipNum).exec(function(err, sites) { 
+    
+	site.find({category: category}).sort('-'+t).limit(25).skip(skipNum).exec(function(err, sites) { 
 		if (sites) {
-			res.render('index', {
+			res.render('categories', {
 		        title: 'cudos',
 		        layout: 'layout',
-		        links: sites
+		        links: sites,
+		        page: page
 		    });
     	} else {
     		console.log("not working")
     		res.end()
     	}
 	});
-};
+};  
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ require( '../db' );
 site = mongoose.model('site')
 
 exports.home = function (req, res) {
-	page = req.query.page
-	skipNum = page + 1 * 25 || 0
+	page = req.query.page || 0
+	skipNum = parseInt(page) * 25 + 1  || 0
+
 	site.find({}).sort('-cudos').limit(25).skip(skipNum).exec(function(err, sites) { 
 		if (sites) {
 			res.render('categories', {
 		        title: 'cudos',
 		        layout: 'layout',
-		        links: sites
+		        links: sites,
+		        page: page
 		    });
     	} else {
     		console.log("not working")
